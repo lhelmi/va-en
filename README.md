fbe3da4f3b0a33b6f013e5fd7f879987607a2c8d
lhelmi <7m.helmi@gmail.com>
2021-02-03 20:06:26 +0700

    add maps

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<img align="left" src="img.png" width="200" height="350">
+<img align="left" src="img.png">
---
+++ core/resources/views/front/maps/index.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Add Map</title>
+    <script
+      src="{{ getenv('Gmaps') }}"
+      defer
+    ></script>
+    <style>
+        /* Set the size of the div element that contains the map */
+        #map {
+            height: 400px;
+            /* The height is 400 pixels */
+            width: 100%;
+            /* The width is the width of the web page */
+        }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+  </body>
+</html>
+<script>
+let map;
+let markers = [];
+
+function initMap() {
+  const haightAshbury = { lat: -25.363, lng: 131.044 };
+  
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: haightAshbury,
+    mapTypeId: "terrain",
+  });
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener("click", (event) => {
+    deleteMarkers();
+    addMarker(event.latLng);
+    console.log(JSON.stringify(event.latLng.toJSON(), null, 2));
+  });
+  // Adds a marker at the center of the map.
+  addMarker(haightAshbury);
+}
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+</script>
